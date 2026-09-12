572b3b64e83d13f079b6511c8ea8557bf200d8bf
Tejaswini Chavvakula <tejaswinich56@gmail.com>
2026-09-12 11:22:11 +0530

    Add linear search

---
+++ 1-Arrays/1-linearsearch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Element found at position " << i + 1 << endl;
+            return 0;
+        }
+    }
+
+    cout << "Element not found" << endl;
+
+    return 0;
+}
